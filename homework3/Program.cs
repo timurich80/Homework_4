f77805d2d3b4512fc2942eed69115b7aa9f8bb8d
timurich80 <timurich80@gmail.com>
2022-09-05 16:06:16 +0300

    код работает

---
+++ homework3/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] 
+*/
+int [] array = new int [8];
+Random rand = new Random();
+Console.Write("[");
+for (int x = 0; x < array.Length; x++)
+      {
+          array[x] = rand.Next(20);
+        
+          Console.Write(array[x] + ", ");
+          
+      }
+Console.Write("]");
