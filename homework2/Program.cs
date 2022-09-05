1278322578e5fc44f373f751be8fae33275051d2
timurich80 <timurich80@gmail.com>
2022-09-05 15:15:30 +0300

    код работат

---
+++ homework2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 
+*/
+
+
+Console.WriteLine("введите число:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int b = a;
+
+while (b > 0)
+{
+int num = b % 10;
+b = b / 10;
+sum = sum + num;
+}
+Console.WriteLine(a + " -> " + sum);
